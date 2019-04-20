dfa97a8fd6b9e305b4c6502800c7fdca0fd987c2
Christopher Doornbos <c3doornb@gmail.com>
2019-04-20 11:54:30 -0700

    Correct table name

--- database/StockChartPostgres.js
+++ database/StockChartPostgres.js
@@ -28,7 +28,7 @@ Stock.init({
 const dataPath = 'c:/Users/chris/HackReactor/SDC/stock-chart/database/data.csv';
 
 sequelize.sync({ force: true })
-  .then(() => sequelize.query(`COPY "Homes" FROM '${dataPath}' DELIMITER('|') CSV HEADER;`)
+  .then(() => sequelize.query(`COPY stocks FROM '${dataPath}' DELIMITER('|') CSV HEADER;`)
     .then(() => console.log('done'))
   );
  
